556d777bb2bfe591a48865f08387f179a420d457
Charles <wkx0823@gmail.com>
2022-09-29 14:43:30 +0800

    Update linked-list.c

--- list/linked-list.c
+++ list/linked-list.c
@@ -34,17 +34,21 @@ void list_insert_by_index(list *, int, data_type);
 int list_empty(list *);
 int list_size(list *);
 void list_reverse(list *);
+void remove_kth_node_from_end(list *, int);
+void remove_duplicates_from_linked_list(list *);
 
 // unit test
 void list_test();
-void remove_kth_node_from_end(list *, int);
 void test_remove_kth_node_from_end();
+void test_remove_duplicates_from_linked_list();
 
 int main()
 {
     list_test();
 
     test_remove_kth_node_from_end();
+
+    test_remove_duplicates_from_linked_list();
 
     return 0;
 }
@@ -271,9 +275,31 @@ void list_recursive(struct node *l)
     list_recursive(l->next);
 }
 
+void remove_duplicates_from_linked_list(list *l)
+{
+    struct node *c = l->head, *n = c->next;
+    while (n != NULL)
+    {
+        if (c->data == n->data)
+        {
+            if (l->tail == n) // handle if the last node is to be removed.
+            {
+                l->tail = c;
+            }
+            c->next = n->next;
+            free(n);
+            n = c->next;
+        }
+        else
+        {
+            c = c->next;
+            n = n->next;
+        }
+    }
+}
+
 void remove_kth_node_from_end(list *l, int k)
 {
-    // implementation
     int i, j = 0;
     struct node *c = l->head;
     while (c != NULL)
@@ -290,13 +316,13 @@ void remove_kth_node_from_end(list *l, int k)
         d = d->next;
         j--;
     }
-    if (p == NULL) // hanlde the head is to be delete.
+    if (p == NULL) // hanlde if the head is to be delete.
     {
         l->head = d->next;
         free(d);
         return;
     }
-    if (d->next == NULL) // handle the tail is to be delete.
+    if (d->next == NULL) // handle if the tail is to be delete.
     {
         l->tail = p;
     }
@@ -306,6 +332,7 @@ void remove_kth_node_from_end(list *l, int k)
 
 void list_test()
 {
+    printf("Running all the tests...\n");
     list *my_list = list_create();
 
     assert(list_size(my_list) == 0 && list_empty(my_list) == LIST_TRUE && my_list->head == NULL && my_list->tail == NULL);
@@ -399,18 +426,150 @@ void list_test()
     list_destory(my_list);
 }
 
+void test_remove_duplicates_from_linked_list()
+{
+    printf("Start running test cases for remove_duplicates_from_linked_list().\n");
+    list *l = list_create();
+
+    /**
+     * @brief Test Case 1.
+     */
+    data_type test_case_1[] = {1, 1, 1, 3, 4, 4, 4, 5, 6, 6};
+    data_type test_case_1_output[] = {1, 3, 4, 5, 6};
+    for (int i = 0; i < sizeof(test_case_1) / sizeof(data_type); i++)
+    {
+        list_insert_back(l, test_case_1[i]);
+    }
+    remove_duplicates_from_linked_list(l);
+    assert(l->head->data == 1 && l->tail->data == 6 && l->tail->next == NULL);
+    for (int i = 0; i < sizeof(test_case_1_output) / sizeof(data_type); i++)
+    {
+        assert(test_case_1_output[i] == list_pop_front(l));
+    }
+    assert(list_empty(l) == LIST_TRUE && list_size(l) == 0 && l->head == NULL && l->tail == NULL);
+    printf("Test Case 1 Passed.\n");
+
+    /**
+     * @brief Test Case 2.
+     */
+    data_type test_case_2[] = {1, 1, 1, 1, 1, 4, 4, 5, 6, 6};
+    data_type test_case_2_output[] = {1, 4, 5, 6};
+    for (int i = 0; i < sizeof(test_case_2) / sizeof(data_type); i++)
+    {
+        list_insert_back(l, test_case_2[i]);
+    }
+    remove_duplicates_from_linked_list(l);
+    assert(l->head->data == 1 && l->tail->data == 6 && l->tail->next == NULL);
+    for (int i = 0; i < sizeof(test_case_2_output) / sizeof(data_type); i++)
+    {
+        assert(test_case_2_output[i] == list_pop_front(l));
+    }
+    assert(list_empty(l) == LIST_TRUE && list_size(l) == 0 && l->head == NULL && l->tail == NULL);
+    printf("Test Case 2 Passed.\n");
+
+    /**
+     * @brief Test Case 3.
+     */
+    data_type test_case_3[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    data_type test_case_3_output[] = {1};
+    for (int i = 0; i < sizeof(test_case_3) / sizeof(data_type); i++)
+    {
+        list_insert_back(l, test_case_3[i]);
+    }
+    remove_duplicates_from_linked_list(l);
+    assert(l->head->data == 1 && l->tail->data == 1 && l->tail->next == NULL);
+    for (int i = 0; i < sizeof(test_case_3_output) / sizeof(data_type); i++)
+    {
+        assert(test_case_3_output[i] == list_pop_front(l));
+    }
+    assert(list_empty(l) == LIST_TRUE && list_size(l) == 0 && l->head == NULL && l->tail == NULL);
+    printf("Test Case 3 Passed.\n");
+
+    /**
+     * @brief Test Case 4.
+     */
+    data_type test_case_4[] = {1, 9, 11, 15, 15, 16, 17};
+    data_type test_case_4_output[] = {1, 9, 11, 15, 16, 17};
+    for (int i = 0; i < sizeof(test_case_4) / sizeof(data_type); i++)
+    {
+        list_insert_back(l, test_case_4[i]);
+    }
+    remove_duplicates_from_linked_list(l);
+    assert(l->head->data == 1 && l->tail->data == 17 && l->tail->next == NULL);
+    for (int i = 0; i < sizeof(test_case_4_output) / sizeof(data_type); i++)
+    {
+        assert(test_case_4_output[i] == list_pop_front(l));
+    }
+    assert(list_empty(l) == LIST_TRUE && list_size(l) == 0 && l->head == NULL && l->tail == NULL);
+    printf("Test Case 4 Passed.\n");
+
+    /**
+     * @brief Test Case 5.
+     */
+    data_type test_case_5[] = {1};
+    data_type test_case_5_output[] = {1};
+    for (int i = 0; i < sizeof(test_case_5) / sizeof(data_type); i++)
+    {
+        list_insert_back(l, test_case_5[i]);
+    }
+    remove_duplicates_from_linked_list(l);
+    assert(l->head->data == 1 && l->tail->data == 1 && l->tail->next == NULL);
+    for (int i = 0; i < sizeof(test_case_5_output) / sizeof(data_type); i++)
+    {
+        assert(test_case_5_output[i] == list_pop_front(l));
+    }
+    assert(list_empty(l) == LIST_TRUE && list_size(l) == 0 && l->head == NULL && l->tail == NULL);
+    printf("Test Case 5 Passed.\n");
+
+    /**
+     * @brief Test Case 6.
+     */
+    data_type test_case_6[] = {-5, -1, -1, -1, 5, 5, 5, 8, 8, 9, 10, 11, 11};
+    data_type test_case_6_output[] = {-5, -1, 5, 8, 9, 10, 11};
+    for (int i = 0; i < sizeof(test_case_6) / sizeof(data_type); i++)
+    {
+        list_insert_back(l, test_case_6[i]);
+    }
+    remove_duplicates_from_linked_list(l);
+    assert(l->head->data == -5 && l->tail->data == 11 && l->tail->next == NULL);
+    for (int i = 0; i < sizeof(test_case_6_output) / sizeof(data_type); i++)
+    {
+        assert(test_case_6_output[i] == list_pop_front(l));
+    }
+    assert(list_empty(l) == LIST_TRUE && list_size(l) == 0 && l->head == NULL && l->tail == NULL);
+    printf("Test Case 6 Passed.\n");
+
+    /**
+     * @brief Test Case 7.
+     */
+    data_type test_case_7[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12};
+    data_type test_case_7_output[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    for (int i = 0; i < sizeof(test_case_7) / sizeof(data_type); i++)
+    {
+        list_insert_back(l, test_case_7[i]);
+    }
+    remove_duplicates_from_linked_list(l);
+    assert(l->head->data == 1 && l->tail->data == 12 && l->tail->next == NULL);
+    for (int i = 0; i < sizeof(test_case_7_output) / sizeof(data_type); i++)
+    {
+        assert(test_case_7_output[i] == list_pop_front(l));
+    }
+    assert(list_empty(l) == LIST_TRUE && list_size(l) == 0 && l->head == NULL && l->tail == NULL);
+    printf("Test Case 7 Passed.\n");
+    printf("Passed all the test cases.\n");
+}
+
 void test_remove_kth_node_from_end()
 {
-    printf("Start running test cases for remove_kth_node_fron_end().\n");
+    printf("\nStart running test cases for remove_kth_node_fron_end().\n");
+    list *l = list_create();
     int k;
     /**
      * @brief Test Case 1.
-     * 
      */
     data_type test_case_1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     data_type test_case_1_output[] = {0, 1, 2, 3, 4, 5, 7, 8, 9};
     k = 4;
-    list *l = list_create();
     for (int i = 0; i < sizeof(test_case_1) / sizeof(data_type); i++)
     {
         list_insert_back(l, test_case_1[i]);
@@ -426,7 +585,6 @@ void test_remove_kth_node_from_end()
 
     /**
      * @brief Test Case 2.
-     * 
      */
     data_type test_case_2[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     data_type test_case_2_output[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
@@ -446,7 +604,6 @@ void test_remove_kth_node_from_end()
 
     /**
      * @brief Test Case 3.
-     * 
      */
     data_type test_case_3[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     data_type test_case_3_output[] = {0, 1, 2, 3, 4, 5, 6, 7, 9};
@@ -466,7 +623,6 @@ void test_remove_kth_node_from_end()
 
     /**
      * @brief Test Case 4.
-     * 
      */
     data_type test_case_4[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     data_type test_case_4_output[] = {0, 1, 2, 3, 4, 5, 6, 8, 9};
@@ -486,7 +642,6 @@ void test_remove_kth_node_from_end()
 
     /**
      * @brief Test Case 5.
-     * 
      */
     data_type test_case_5[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     data_type test_case_5_output[] = {0, 1, 2, 3, 4, 6, 7, 8, 9};
@@ -506,7 +661,6 @@ void test_remove_kth_node_from_end()
 
     /**
      * @brief Test Case 6.
-     * 
      */
     data_type test_case_6[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     data_type test_case_6_output[] = {0, 1, 2, 3, 5, 6, 7, 8, 9};
@@ -526,7 +680,6 @@ void test_remove_kth_node_from_end()
 
     /**
      * @brief Test Case 7.
-     * 
      */
     data_type test_case_7[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     data_type test_case_7_output[] = {0, 1, 2, 4, 5, 6, 7, 8, 9};
@@ -546,7 +699,6 @@ void test_remove_kth_node_from_end()
 
     /**
      * @brief Test Case 8.
-     * 
      */
     data_type test_case_8[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     data_type test_case_8_output[] = {0, 1, 3, 4, 5, 6, 7, 8, 9};
@@ -566,7 +718,6 @@ void test_remove_kth_node_from_end()
 
     /**
      * @brief Test Case 9.
-     * 
      */
     data_type test_case_9[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     data_type test_case_9_output[] = {0, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -586,7 +737,6 @@ void test_remove_kth_node_from_end()
 
     /**
      * @brief Test Case 10.
-     * 
      */
     data_type test_case_10[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     data_type test_case_10_output[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -603,5 +753,5 @@ void test_remove_kth_node_from_end()
     }
     assert(list_empty(l) == LIST_TRUE && list_size(l) == 0 && l->head == NULL && l->tail == NULL);
     printf("Test Case 10 Passed.\n");
-    printf("Passed all the test cases.\n");
+    printf("Passed all the test cases.\n\n");
 }
