8f2b420e8aabd233b9136170106494401cd0f426
Charles <wkx0823@gmail.com>
2022-10-10 16:31:55 +0800

    Update stack_test.h

--- stack/stack_test.h
+++ stack/stack_test.h
@@ -2,5 +2,10 @@
 #include <assert.h>
 #include <string.h>
 
+#ifndef __PROJECT_STACK_LINKED_LIST_TEST_H__
+#define __PROJECT_STACK_LINKED_LIST_TEST_H__
+
 void test_all();
 void test_balanced_brackets();
+
+#endif
