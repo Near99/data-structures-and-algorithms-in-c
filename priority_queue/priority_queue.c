0536b9e0ce6bed166dadb341714a9cb8919cc5e8
Charles <wkx0823@gmail.com>
2022-10-21 14:27:41 +0800

    Update priority_queue.c

--- priority_queue/priority_queue.c
+++ priority_queue/priority_queue.c
@@ -17,32 +17,35 @@ typedef struct priority_queue
 Priority_Queue *p_queue_create(int size);
 void p_queue_insert(Priority_Queue *Q, int x);
 int p_queue_find_min(Priority_Queue *Q);
-void p_queue_delete_min(Priority_Queue *Q);
+int p_queue_extract_min(Priority_Queue *Q);
 int p_queue_size(Priority_Queue *Q);
 int p_queue_is_empty(Priority_Queue *Q);
 int p_queue_is_full(Priority_Queue *Q);
 int p_queue_parent(int index);
 int p_queue_young_child(int index);
 void p_queue_bubble_up(Priority_Queue *Q, int index);
+void p_queue_bubble_down(Priority_Queue *Q, int index); // percolate down operation also called heapify
 /**
  * heapsort is nothing but an implementation of selection sort using the right data structre. 
  */
-void heap_sort();
-
+void heap_sort(int s[], int n);
+void make_heap(Priority_Queue *Q, int s[], int n);
 void pq_print(Priority_Queue *Q);
 
 int main()
 {
     Priority_Queue *q = p_queue_create(10);
-    p_queue_insert(q, 10);
-    p_queue_insert(q, 20);
-    p_queue_insert(q, 25);
-    p_queue_insert(q, 35);
-    p_queue_insert(q, 13);
-    p_queue_insert(q, 15);
-    p_queue_insert(q, 18);
-    p_queue_insert(q, 9);
-    pq_print(q);
+    int arr[] = {200, 2, 19, 55, 70, 34, 28, 25, 66, 44};
+    int heapsort[10];
+    make_heap(q, arr, 10);
+    for (int i = 0; i < 10; i++)
+    {
+        heapsort[i] = p_queue_extract_min(q);
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", heapsort[i]);
+    }
     return 0;
 }
 
@@ -66,6 +69,19 @@ int p_queue_find_min(Priority_Queue *Q)
         exit(EXIT_FAILURE);
     }
     return Q->queue[0];
+}
+
+int p_queue_extract_min(Priority_Queue *Q)
+{
+    if (p_queue_is_empty(Q))
+    {
+        printf("Empty queue!\n");
+        exit(EXIT_FAILURE);
+    }
+    int min = Q->queue[0];
+    Q->queue[0] = Q->queue[--Q->size];
+    p_queue_bubble_down(Q, 0);
+    return min;
 }
 
 void p_queue_insert(Priority_Queue *Q, int x)
@@ -114,10 +130,44 @@ void p_queue_bubble_up(Priority_Queue *Q, int index)
     }
 }
 
+void p_queue_bubble_down(Priority_Queue *Q, int p)
+{
+    // min
+    int m = p;
+    // child
+    int c = p_queue_young_child(m);
+    // loop twich to find which child is smaller.
+    for (int i = 0; i <= 1; i++)
+    {
+        if ((c + i) < Q->size) // boundary check.
+        {
+            if (Q->queue[m] > Q->queue[c + i])
+            {
+                m = c + i;
+            }
+        }
+    }
+    if (m != p)
+    {
+        int temp = Q->queue[m];
+        Q->queue[m] = Q->queue[p];
+        Q->queue[p] = temp;
+        p_queue_bubble_down(Q, m);
+    }
+}
+
 void pq_print(Priority_Queue *Q)
 {
     for (int i = 0; i < Q->capacity; i++)
     {
         printf("%d\n", Q->queue[i]);
+    }
+}
+
+void make_heap(Priority_Queue *Q, int s[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        p_queue_insert(Q, s[i]);
     }
 }
